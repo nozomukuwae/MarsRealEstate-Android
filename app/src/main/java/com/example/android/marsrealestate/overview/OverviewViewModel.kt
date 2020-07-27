1bdcc1c6c0797b53039a816e6c2df4036358d250
Nozomu Kuwae <nozomukuwae@gmail.com>
2020-07-27 21:54:46 +0900

    Adopt coroutines

--- app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -22,6 +22,10 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.android.marsrealestate.network.MarsApi
 import com.example.android.marsrealestate.network.MarsProperty
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -38,6 +42,8 @@ class OverviewViewModel : ViewModel() {
     val response: LiveData<String>
         get() = _response
 
+    private var job = Job()
+    private val coroutineScope = CoroutineScope(Dispatchers.Main + job)
     /**
      * Call getMarsRealEstateProperties() on init so we can display status immediately.
      */
@@ -45,19 +51,23 @@ class OverviewViewModel : ViewModel() {
         getMarsRealEstateProperties()
     }
 
+    override fun onCleared() {
+        super.onCleared()
+        job.cancel()
+    }
+
     /**
      * Sets the value of the status LiveData to the Mars API status.
      */
     private fun getMarsRealEstateProperties() {
-        MarsApi.retrofitService.getProperties().enqueue(object: Callback<List<MarsProperty>>{
-            override fun onResponse(call: Call<List<MarsProperty>>, response: Response<List<MarsProperty>>) {
-                _response.value = "Success: ${response.body()?.size} Mars properties retrieved"
-            }
-
-            override fun onFailure(call: Call<List<MarsProperty>>, t: Throwable) {
+        coroutineScope.launch {
+            var getPropertiesDeferred = MarsApi.retrofitService.getProperties()
+            try {
+                var list = getPropertiesDeferred.await()
+                _response.value = "Success: ${list.size} Mars properties retrieved"
+            } catch (t:Throwable) {
                 _response.value = "Failure: ${t.message}"
             }
-        })
-        _response.value = "Set the Mars API Response here!"
+        }
     }
 }
